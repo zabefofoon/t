0c71d4f9182959525cc914842cd1ea8fdf678ed4
zabefofoon <37139656+zabefofoon@users.noreply.github.com>
2019-11-19 05:06:07 +0900

    Add files via upload

---
+++ precache-manifest.f4bef4fa18247e2a35c01d17a504674a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2e08e15e0dd2fc2d82cebdabc86802",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a9bbaad27fc89178296",
+    "url": "./static/css/main.4cf72d14.chunk.css"
+  },
+  {
+    "revision": "d4f794bc2a706309f8fe",
+    "url": "./static/js/2.28a2c77b.chunk.js"
+  },
+  {
+    "revision": "4a9bbaad27fc89178296",
+    "url": "./static/js/main.60de2258.chunk.js"
+  },
+  {
+    "revision": "3bcd3ee74d56dd4f841b",
+    "url": "./static/js/runtime-main.bd28b82b.js"
+  },
+  {
+    "revision": "60b69a0e7160dacd380fea8b5f303ae8",
+    "url": "./static/media/b2.60b69a0e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.311da6a4e3795df7c27e724c73b450a2.js"
+  "./precache-manifest.f4bef4fa18247e2a35c01d17a504674a.js"
 );
 
 self.addEventListener('message', (event) => {
